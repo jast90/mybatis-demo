2431fc535bdde6f642a63d78ad3a0ca9edbaee8d
zhangzhiwen <zhangzhiwen91@gmail.com>
2018-01-22 10:53:19 +0800

    静态资源改成webjar

---
+++ src/main/java/cn/jastz/mybatis/demo/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package cn.jastz.mybatis.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author zhiwen
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
